f3ea17440dbedb089d748f49b036ff5418873419
mayingchun <mayingchun321@outlook.com>
2019-02-06 23:01:56 -0800

    project1A half finished

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,126 @@
+public class LinkedListDeque<mytype> {
+    public class Node {
+        public Node prev;
+        public Node next;
+        public mytype item;
+        public Node(mytype a, Node p, Node n) {
+            prev = p;
+            next = n;
+            item = a;
+        }
+        /* use this when first create the Deque*/
+        public Node() {
+            prev = null;
+            next = null;
+            item = null;
+        }
+    }
+
+    private Node sentinelf;
+    private Node sentinell;
+    private int size;
+
+    public boolean isEmpty() {
+        if(size == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public LinkedListDeque() {
+        sentinelf = new Node();
+        sentinell = new Node();
+        sentinelf.next = sentinell;
+        sentinell.prev = sentinelf;
+        size = 0;
+    }
+
+    public LinkedListDeque( LinkedListDeque<mytype> l) {
+        sentinelf = new Node();
+        sentinell = new Node();
+        sentinelf.next = sentinell;
+        sentinell.prev = sentinelf;
+        size = 0;
+        int i = 0;
+        while(i < l.size()) {
+            addLast(l.get(i));
+            i++;
+            size++;
+        }
+    }
+
+    public void addFirst(mytype item) {
+        sentinelf.next = new Node(item, sentinelf, sentinelf.next);
+        sentinelf.next.next.prev = sentinelf.next;
+        size = size + 1;
+    }
+
+    public void addLast(mytype item) {
+        sentinell.prev = new Node(item, sentinell.prev, sentinell);
+        sentinell.prev.prev.next = sentinell.prev;
+        size = size + 1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        Node p = sentinelf.next;
+        while (p != sentinell) {
+            System.out.print(p.item + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+    public mytype removeFirst() {
+        if(isEmpty() == true) {
+            return null;
+        }
+        mytype item = sentinelf.next.item;
+        sentinelf.next.next.prev = sentinelf;
+        sentinelf.next = sentinelf.next.next;
+        size --;
+        return item;
+    }
+
+    public mytype removeLast() {
+        if(isEmpty() == true) {
+            return null;
+        }
+        mytype item = sentinell.prev.item;
+        sentinell.prev.prev.next = sentinell;
+        sentinell.prev = sentinell.prev.prev;
+        size --;
+        return item;
+    }
+
+    public mytype get(int index) {
+        if(index >= size()) {
+            return null;
+        }
+        Node p = sentinelf.next;
+        while (index > 0) {
+            p = p.next;
+            index = index -1;
+        }
+        return p.item;
+    }
+
+    private mytype Recursive(int index, Node p) {
+        if(index == 0) {
+            return p.item;
+        }
+        return Recursive(index - 1, p.next);
+    }
+    public mytype getRecursive(int index) {
+        if(index >= size()) {
+            return null;
+        }
+        return Recursive(index, sentinelf.next);
+    }
+
+}
